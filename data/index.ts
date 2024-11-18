f72db9e1ee49590ab2b7cac104579ba4110233ee
Muhammad Mudasir <167820839+muhammadmudasirawan@users.noreply.github.com>
2024-11-19 04:20:35 +0500

    change Footer.tsx

--- data/index.ts
+++ data/index.ts
@@ -207,13 +207,16 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
+    link: "https://github.com/muhammadmudasirawan",
   },
   {
     id: 2,
     img: "/twit.svg",
+    link: "https://www.facebook.com/your-profile",
   },
   {
     id: 3,
     img: "/link.svg",
+    link: "https://www.linkedin.com/in/muhammad-mudasirsir-68abb32a8/",
   },
 ];
